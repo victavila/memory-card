c6d98d71e8a4fa7e33cd26b7da7e307e60ca2b74
Victor Avila <victoravila1997@gmail.com>
2022-07-01 15:40:58 -0500

    Created and added header

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header () {
+    return (
+        <header className="header">
+            <div>
+                <h1 className="title">One Piece Memory Game</h1>
+            </div>
+            <div className="instructions">
+                <p>Get points by clicking on One Piece characters. Don't click on them more than once. Max score is 12 points.</p>
+            </div>
+        </header>
+    )
+}
