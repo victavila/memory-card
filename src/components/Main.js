904b2d6b44a8ad34773082705daf7f91a16bfcb3
Victor Avila <victoravila1997@gmail.com>
2022-07-01 15:58:31 -0500

    Added logic to fetch character information from jikan API

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,28 @@
+import React, { useEffect, useState} from "react";
 
+const jikanjs  = require('jikanjs');
 
 export default function Main () {
-    return (
-        <main className="main"></main>
-    )
+
+  const [characters, setCharacters] = useState([]);
+
+  const fetchCharacters = async () => {
+    const response = await jikanjs.loadAnime(21, 'characters_staff');
+    const characters = await response.characters.filter(character => character.role === "Main" || character.name === "Trafalgar, Law" || character.name === "Portgas D., Ace")
+    characters.forEach(character => {
+      setCharacters((prevArr) => [...prevArr, {name: character.name, image: character.image_url}])
+    })
+  }
+
+  useEffect(() => {
+    fetchCharacters();
+  }, [])
+
+  
+
+  
+    
+  return (
+    <main className="main"></main>
+  )
 }
